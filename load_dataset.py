bcaebd3723d8273b616d35595f707eaabc21f68e
GaoJiaLiangLreis <43036052+GaoJiaLiangLreis@users.noreply.github.com>
2023-09-01 10:26:00 +0800

    Add files via upload

--- load_dataset.py
+++ load_dataset.py
@@ -15,7 +15,7 @@ class Tourism_KG_Dataset(DGLDataset):
     def process(self):
         node_id_dict = dict()
         node_idx = 0
-        bh_graph_nx = nx.read_gexf('data/{}_graph_1.0.gexf'.format(self.city)).to_directed()
+        bh_graph_nx = nx.read_gexf('data/{}_graph_1.1.gexf'.format(self.city)).to_directed()
         for node in bh_graph_nx.nodes:
             node_id_dict[node] = node_idx
             node_idx += 1
@@ -25,15 +25,19 @@ class Tourism_KG_Dataset(DGLDataset):
         src_ids = []
         dst_ids = []
         edge_weights = []
+        edge_dists = []
         for edge in bh_graph_nx.edges:
             src, dst = edge
             weight = bh_graph_nx.edges[edge]['weight']
+            dist = bh_graph_nx.edges[edge]['distance']
             src_ids.append(node_id_dict[src])
             dst_ids.append(node_id_dict[dst])
             edge_weights.append(weight)
+            edge_dists.append(dist)
 
         bh_graph_dgl = dgl.graph((src_ids, dst_ids), num_nodes=bh_graph_nx.number_of_nodes())
         bh_graph_dgl.edata['weight'] = th.tensor(edge_weights)
+        bh_graph_dgl.edata['dist'] = th.tensor(edge_dists)
         # print(bh_graph_dgl)
 
         with open('data/embed_Beijing.vec', 'r', encoding='utf-8') as file:
@@ -47,15 +51,17 @@ class Tourism_KG_Dataset(DGLDataset):
         features_map = []
         for poi_id in node_id_dict:
             entity = 'attr/' + poi_id
-            ent_idx = entity_idx_in_TKG[entity]['node_idx']
-            features_map.append(features[ent_idx].numpy())
+            if entity in entity_idx_in_TKG:
+                ent_idx = entity_idx_in_TKG[entity]['node_idx']
+                features_map.append(features[ent_idx].numpy())
         features_map = th.tensor(features_map)
         # print(features_map.shape)
 
         self.graph = bh_graph_dgl
 
+        # self.graph.ndata["semantic_feat"] = features_map
         self.graph.ndata["semantic_feat"] = features_map
-        self.graph.ndata['spatial_feat'] = th.load('data/spatial_feats_Beijing.pt')
+        self.graph.ndata['spatial_feat'] = th.load('data/{}_spatial_context.pt'.format(self.city))
 
         train_df = pd.read_csv('./data/dataset_{}/train.csv'.format(self.city), sep=',', encoding='utf-8')
         test_df = pd.read_csv('./data/dataset_{}/test.csv'.format(self.city), sep=',', encoding='utf-8')
@@ -94,6 +100,15 @@ class Tourism_KG_Dataset(DGLDataset):
         self.test_pos_g = dgl.graph((test_pos_u, test_pos_v), num_nodes=self.graph.num_nodes())
         self.test_neg_g = dgl.graph((test_neg_u, test_neg_v), num_nodes=self.graph.num_nodes())
 
+        all_nodes = self.graph.nodes()
+        all_u = []
+        all_v = []
+        for u in all_nodes:
+            for v in all_nodes:
+                all_u.append(u)
+                all_v.append(v)
+        self.graph_all_pairs = dgl.graph((all_u, all_v), num_nodes=self.graph.num_nodes())
+
     def __getitem__(self, i):
         if i == 0:
             return self.graph
@@ -101,9 +116,11 @@ class Tourism_KG_Dataset(DGLDataset):
             return self.train_pos_g, self.train_neg_g
         elif i == 2:
             return self.test_pos_g, self.test_neg_g
+        elif i == 3:
+            return self.graph_all_pairs
 
     def __len__(self):
-        return 3
+        return 4
 
 
 if __name__ == '__main__':
